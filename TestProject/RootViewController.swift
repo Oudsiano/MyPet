1ce1cde9fe4765f76f2e0192901ce7c72306c1ea
Marat <mar.davidoff@gmail.com>
2023-09-11 06:59:08 +0300

    FirstCommit

---
+++ TestProject/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Natalie Mikheeva on 04.05.2023.
+//
+
+import UIKit
+
+final class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
